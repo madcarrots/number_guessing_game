9ce8344b272dafaf078d260a4feb698b83fdc1a6
Thomas Zermeno <madcarrots@users.noreply.github.com>
2026-04-30 01:50:58 -0400

    feat: assign variables from query

--- number_guess.sh
+++ number_guess.sh
@@ -7,7 +7,7 @@ PSQL="psql -q --username=freecodecamp --dbname=number_guess -t -A -F '|'"
 GENERATED_NUMBER=$(( (RANDOM % 1001) + 1 ))
 echo "$GENERATED_NUMBER"
 
-echo -e '\nEnter your username:'
+echo -e 'Enter your username:'
 read USER
 
 # convert username to all caps
@@ -47,6 +47,18 @@ if [ -z "$GAME_INFO" ]; then
 
   echo "Welcome, $USERNAME! It looks like this is your first time here." 
 else
+  # Parse the pipe-separated result into variables
+  # variables match the order of the query items
+  IFS='|' read -r USERNAME NUMBER_OF_GUESSES SECRET_NUMBER GAMES_PLAYED BEST_GAME <<< "$GAME_INFO"
+
+  # Remove any single quotes
+  USERNAME="${USERNAME//\'/}"
+  NUMBER_OF_GUESSES="${NUMBER_OF_GUESSES//\'/}"
+  SECRET_NUMBER="${SECRET_NUMBER//\'/}"
+  GAMES_PLAYED="${GAMES_PLAYED//\'/}"
+  BEST_GAME="${BEST_GAME//\'/}"
+
+  # Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses.
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
