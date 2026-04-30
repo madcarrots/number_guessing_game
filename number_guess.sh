d910c580eb1898b6c15760e953d0d47f95c579b2
Thomas Zermeno <madcarrots@users.noreply.github.com>
2026-04-29 22:52:24 -0400

    feat: add query

--- number_guess.sh
+++ number_guess.sh
@@ -7,4 +7,24 @@ echo $GENERATED_NUMBER
 echo -e '\nEnter your username:'
 read USERNAME
 
+# convert username to all caps
+
+# Use converted username as WHERE
+
+# Create query 
+QUERY="
+  SELECT
+    username,
+    number_of_guesses,
+    secret_number,
+    games_played,
+    best_game
+  FROM
+    records
+  WHERE $WHERE;  
+"
+# If WHERE not found
+
+# Add WHERE to database
+
 echo $USERNAME
